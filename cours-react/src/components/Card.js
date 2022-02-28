f3e3a7f851e9ea2328ab306101dcd554c2a7bc53
laurent972 <laurent.digitick@gmail.com>
2022-02-28 17:59:49 +0100

    Finex

--- cours-react/src/components/Card.js
+++ cours-react/src/components/Card.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => { 
+  const {country} = props;
+
+ const numberFormat = (num) => {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+ }
+
   return (
-    <div className="card">
-      Card
-    </div>
+    <li className="card">
+      <img src={country.flags.png} alt="flag" /> 
+      <div className="data-container">
+        <ul>
+          <li>{country.name.common}</li>
+          <li>{country.capital}</li>
+          <li>Pop. : {numberFormat(country.population)}</li>
+        </ul>
+      </div>
+    
+     
+    </li>
   );
 };
 
--- cours-react/src/components/Countries.js
+++ cours-react/src/components/Countries.js
@@ -3,25 +3,64 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 
-
+// Fetch
 const Countries = () => {
 
   const [data, setData] = useState([]);
+  const [sortedData, setSortedData] = useState([]);
+  const [playOnce, setPlayOnce] = useState([]);
+  const [rangeValue, setRangeValue] = useState(40);
+  const [selectedRadio, setSelectedRadio] = useState('');
+  const radios = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
 
   useEffect(()=>{
-    axios
-    .get('https://restcountries.com/v3.1/all')
-    .then((res)=> setData(res.data));
-    console.log(data);
-  },[]);
+    if(playOnce){ // statut est à true
+      axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((res)=> {
+        setData(res.data);
+        setPlayOnce(false); // set Statut à false
+      })
+    }
+    
+    const sortedCountry = () => {
+      const countryObj = Object.keys(data).map((i)=>data[i]);
+      const sortedArray = countryObj.sort((a,b) => {
+        return b.population - a.population
+      });
+      sortedArray.length = rangeValue;
+      setSortedData(sortedArray);
+    }
+    sortedCountry();
+  },[data, playOnce, rangeValue]); // callback, relance du use effect, avec les nouvelles données
 
 
   return (
    
    <div className="countries">
+     <div className="radio-container">
+       <input type="range" min="1" max="250" vamue={rangeValue} onChange={(e)=> setRangeValue(e.target.value)}/>
+
+       <ul className="radio-display">
+         {radios.map((radio)=>{
+           return(
+             <li key={radio}>
+               <input type="radio" value={radio} id={radio} checked={radio === selectedRadio } onChange={(e)=> setSelectedRadio(e.target.value)}/>
+               <label htmlFor={radio}>{radio}</label>
+             </li>
+           )
+         })}
+       </ul>
+     </div>
+     
+       {selectedRadio &&  <div className="cancel" onClick={()=> setSelectedRadio("")}><h5>Annuler la recherche</h5></div>}
+     
      <ul className="countries-list">
-        {data.map((country)=> (
-           <Card />
+        {sortedData
+        .filter((country) => country.region.includes(selectedRadio))
+        .map((country)=> (
+          <Card country={country} key={country.name.common}/>
+
         ))}
      </ul>
    </div>
